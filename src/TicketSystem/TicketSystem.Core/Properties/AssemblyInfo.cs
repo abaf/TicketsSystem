c6b17b218b5911e0fadd8603196544aaa5de8ae4
jianliu <svk-sky@126.com>
2016-03-26 11:52:15 +0800

    create threadcontext to store the route

--- src/TicketSystem/TicketSystem.Core/Properties/AssemblyInfo.cs
+++ src/TicketSystem/TicketSystem.Core/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("TicketSytem.Core")]
+[assembly: AssemblyTitle("TicketSystem.Core")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("TicketSytem.Core")]
+[assembly: AssemblyProduct("TicketSystem.Core")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
--- src/TicketSystem/TicketSystem.ThreadManager/ThreadContext.cs
+++ src/TicketSystem/TicketSystem.ThreadManager/ThreadContext.cs
@@ -3,11 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using TicketSytem.Core
+using TicketSystem.Core;
 namespace TicketSystem.ThreadManager
 {
     public class ThreadContext
     {
-        public static 
+        public static Route Route { get; set; }
     }
 }

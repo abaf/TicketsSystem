6fc8f1c9ca31450f44aed947bf9faacb031ba255
jianliu <svk-sky@126.com>
2016-03-26 11:54:45 +0800

    ticket message

---
+++ src/TicketSystem/TicketSystem.Message/BuyTicketResponse.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicketSystem.Message
+{
+    public class BuyTicketResponse: Message
+    {
+        public string Route { get; set; }
+        public string StartStation { get; set; }
+        public string EndStation { get; set; }
+        /// <summary>
+        /// Actual tickets bought
+        /// </summary>
+        public int Tickets { get; set; }
+    }
+}
